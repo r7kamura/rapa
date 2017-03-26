aa81469dd9e3333329cb304856c64455b551cc6d
r7kamura <r7kamura@gmail.com>
2017-03-26 15:38:20 +0900

    Refactor test

--- test/test_search_items_query.rb
+++ test/test_search_items_query.rb
@@ -6,34 +6,40 @@ require "timecop"
 class TestSearchItemsQuery < Petitest::Test
   prepend ::Petitest::PowerAssert
 
-  # @return [Rapa::Queries::SearchItemsQuery]
-  def query
-    ::Rapa::Queries::SearchItemsQuery.new(
+  def test_to_hash
+    freeze_time
+    expected = {
+      "AssociateTag" => "dummy_associate_tag",
+      "AWSAccessKeyId" => "dummy_access_key_id",
+      "Keywords" => "dummy_keyword1%2Cdummy_keyword2",
+      "Operation" => "ItemSearch",
+      "SearchIndex" => "All",
+      "Service" => "AWSECommerceService",
+      "Timestamp" => "2000-01-01T00%3A00%3A00Z",
+      "Version" => "2013-08-01",
+    }
+    actual = ::Rapa::Queries::SearchItemsQuery.new(
       access_key_id: "dummy_access_key_id",
       associate_tag: "dummy_associate_tag",
       keywords: %w(
         dummy_keyword1
         dummy_keyword2
       )
-    )
+    ).to_hash
+    assert do
+      actual == expected
+    end
+    return_time
   end
 
-  def test_to_hash
+  private
+
+  def freeze_time
     now = ::Time.utc(2000)
-    ::Timecop.freeze(now) do
-      expected = {
-        "AssociateTag" => "dummy_associate_tag",
-        "AWSAccessKeyId" => "dummy_access_key_id",
-        "Keywords" => "dummy_keyword1%2Cdummy_keyword2",
-        "Operation" => "ItemSearch",
-        "SearchIndex" => "All",
-        "Service" => "AWSECommerceService",
-        "Timestamp" => "2000-01-01T00%3A00%3A00Z",
-        "Version" => "2013-08-01",
-      }
-      assert do
-        query.to_hash == expected
-      end
-    end
+    ::Timecop.freeze(now)
+  end
+
+  def return_time
+    ::Timecop.return
   end
 end

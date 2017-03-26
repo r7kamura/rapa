74b362c195135bb35822df661d65dea3ed83d5d9
r7kamura <r7kamura@gmail.com>
2017-03-26 15:34:35 +0900

    Add tests

--- rapa.gemspec
+++ rapa.gemspec
@@ -21,5 +21,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency "faraday_middleware"
   spec.add_dependency "multi_xml"
   spec.add_development_dependency "bundler", "~> 1.13"
+  spec.add_development_dependency "petitest", "~> 0.3.1"
+  spec.add_development_dependency "petitest-power_assert"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "timecop"
 end
---
+++ test/rapa_queries_search_items_query_test.rb
@@ -0,0 +1,39 @@
+require "petitest/autorun"
+require "petitest/power_assert"
+require "rapa"
+require "timecop"
+
+class RapaQueriesSearchItemsQueryTest < Petitest::Test
+  prepend ::Petitest::PowerAssert
+
+  # @return [Rapa::Queries::SearchItemsQuery]
+  def query
+    ::Rapa::Queries::SearchItemsQuery.new(
+      access_key_id: "dummy_access_key_id",
+      associate_tag: "dummy_associate_tag",
+      keywords: %w(
+        dummy_keyword1
+        dummy_keyword2
+      )
+    )
+  end
+
+  def test_to_hash
+    now = ::Time.utc(2000)
+    ::Timecop.freeze(now) do
+      expected = {
+        "AssociateTag" => "dummy_associate_tag",
+        "AWSAccessKeyId" => "dummy_access_key_id",
+        "Keywords" => "dummy_keyword1%2Cdummy_keyword2",
+        "Operation" => "ItemSearch",
+        "SearchIndex" => "All",
+        "Service" => "AWSECommerceService",
+        "Timestamp" => "2000-01-01T00%3A00%3A00Z",
+        "Version" => "2013-08-01",
+      }
+      assert do
+        query.to_hash == expected
+      end
+    end
+  end
+end

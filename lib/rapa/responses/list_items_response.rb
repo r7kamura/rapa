19c6465490b375a897df48198993a3682dcc05cb
tetsuya <tetsuya@users.noreply.github.com>
2017-08-23 20:25:54 +0900

    Add error handling to ListItemsResponse

--- lib/rapa/responses/list_items_response.rb
+++ lib/rapa/responses/list_items_response.rb
@@ -1,6 +1,18 @@
 module Rapa
   module Responses
     class ListItemsResponse < BaseResponse
+      # @return [Rapa::Error]
+      def error
+        if value = body.dig("ItemLookupErrorResponse", "Error")
+          ::Rapa::Error.new(value)
+        end
+      end
+
+      # @return [Boolean]
+      def has_error?
+        !error.nil?
+      end
+
       private
 
       # @note Override
